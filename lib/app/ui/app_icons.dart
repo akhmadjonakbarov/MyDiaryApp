9ee03b853bd841ff3fcb833e9ec793d9481d71a4
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-02-04 05:27:43 +0500

    ref: colors were changed

--- lib/app/ui/app_icons.dart
+++ lib/app/ui/app_icons.dart
@@ -1,10 +1,12 @@
 class AppIcons {
   static const format = "svg";
   static const mainPath = "assets/svgs";
-
   static const String about = "$mainPath/about.$format";
   static const String myEntries = "$mainPath/my_entries.$format";
   static const String newEntry = "$mainPath/new_entry.$format";
   static const String settings = "$mainPath/settings.$format";
   static const String arrowBack = "$mainPath/arrow_back.$format";
+  static const String tag = "$mainPath/tag.$format";
+  static const String trash = "$mainPath/trash.$format";
+  static const String edit = "$mainPath/edit.$format";
 }
