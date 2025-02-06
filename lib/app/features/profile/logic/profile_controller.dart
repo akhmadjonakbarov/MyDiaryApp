88c0e604110581ac79960adf684b23e8ec6dac1e
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2025-02-07 03:17:30 +0500

    feat: profile_controller.dart was done

--- lib/app/features/profile/logic/profile_controller.dart
+++ lib/app/features/profile/logic/profile_controller.dart
@@ -1,7 +1,54 @@
-import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
+import '../../../core/storage/shared_prefs_helper.dart';
 import '../models/profile.dart';
 
-class ProfileController extends GetxController {}
+class ProfileController extends GetxController {
+  var profile = Profile.empty().obs;
+
+  SharedPrefsHelper sharedPrefsHelper = Get.find<SharedPrefsHelper>();
+
+  @override
+  void onInit() {
+    super.onInit();
+    getProfile();
+  }
+
+  void getProfile() {
+    String fullName =
+        sharedPrefsHelper.getString(SharedPreferencesKeys.fullName.name) ?? "";
+    String? imagePath =
+        sharedPrefsHelper.getString(SharedPreferencesKeys.imagePath.name);
+    String status =
+        sharedPrefsHelper.getString(SharedPreferencesKeys.status.name) ?? "";
+    String about =
+        sharedPrefsHelper.getString(SharedPreferencesKeys.about.name) ?? "";
+
+    profile.value = Profile(
+      fullName: fullName,
+      imagePath: imagePath,
+      status: status,
+      about: about,
+    );
+  }
+
+  void setProfile(String key, var value) {
+    switch (key) {
+      case 'fullName':
+        sharedPrefsHelper.setString(key, value);
+        break;
+      case 'imagePath':
+        sharedPrefsHelper.setString(key, value);
+        break;
+      case 'status':
+        sharedPrefsHelper.setString(key, value);
+        break;
+      case 'about':
+        sharedPrefsHelper.setString(key, value);
+        break;
+      default:
+        throw ArgumentError('Invalid profile key: $key');
+    }
+    getProfile();
+  }
+}
